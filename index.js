992157fcd31f0139f8b1a194447f0198e054caf7
labamnus <labamnus@gmail.com>
2021-05-07 16:23:10 +0600

    Tests commit
    - Add test model

--- index.js
+++ index.js
@@ -4,10 +4,8 @@ import compression from 'compression';
 import helmet from 'helmet';
 import notFound from './src/middlewares/notFound';
 import errorHandler from './src/middlewares/errorHandler';
-import usersRouter from './src/routes/users';
 import authRouter from './src/routes/auth';
-import { postsRouter } from './src/routes/posts';
-import authMiddleware from './src/middlewares/auth';
+import userRouter from './src/routes/user';
 
 const app = express();
 
@@ -17,8 +15,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/auth', authRouter);
-app.use('/users', authMiddleware, usersRouter);
-app.use('/posts', authMiddleware, postsRouter);
+app.use('/user', userRouter);
 
 app.get('/ping', (req, res) => {
     return res.status(200).json('pong');
---
+++ src/models/Test.js
@@ -0,0 +1,58 @@
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+
+const testSchema = new Schema({
+    type: {
+        type: String,
+        enum: ['country'],
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        default: '',
+    },
+    author: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User',
+    },
+    questions: [
+        {
+            title: {
+                type: String,
+                required: true,
+            },
+            description: {
+                type: String,
+                required: true,
+                maxLength: 1500,
+            },
+            answers: [
+                {
+                    text: {
+                        type: String,
+                        required: true,
+                    },
+                    location: {
+                        type: {
+                            type: String,
+                            enum: ['Point'],
+                            required: true,
+                        },
+                        coordinates: {
+                            type: [Schema.Types.Number],
+                            required: true,
+                        },
+                    },
+                },
+            ],
+        },
+    ],
+});
+
+const Test = mongoose.model('Test', testSchema);
+
+export default Test;
